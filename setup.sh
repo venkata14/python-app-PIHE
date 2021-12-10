cf3adf2badc90ff0afefdbfca8611531f9f2376f
venkatapatchigolla <p.venkata32@gmail.com>
2021-12-10 17:15:14 -0500

    added env setup

--- setup.sh
+++ setup.sh
@@ -6,3 +6,10 @@ port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
+
+echo "\
+[general]\n\
+email = \"venkata.patchigolla@uconn.edu\"\n\
+" > ~/.streamlit/credentials.toml 
+
+echo "API_KEY = \"AIzaSyAPgf37h-eTSW5SiBG28gpiRnghfZOwAY4\"" > ~/.streamlit/secrets.toml
